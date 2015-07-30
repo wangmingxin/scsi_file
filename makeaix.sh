03dd4b81a6c2e01d4ed29b93a4d3187390953102
wang <wang@localhost.localdomain>
2015-07-30 02:49:34 -0700

    all

--- makeaix.sh
+++ makeaix.sh
@@ -3,7 +3,7 @@
 aixhost=198.88.88.38
 ssh $aixhost rm /tmp/scsi_file
 scp -r . $aixhost:/tmp/scsi_file 
-ssh $aixhost cd /tmp/scsi_file/src/;gmake -f Makefile.aix
+ssh $aixhost "cd /tmp/scsi_file/src/;gmake -f Makefile.aix clean;gmake -f Makefile.aix"
 scp $aixhost:/tmp/scsi_file/src/*.a .
 scp $aixhost:/tmp/scsi_file/src/*.so .
 
